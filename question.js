3411e843f343b8cd93a8a0cddc1f4ec233a474f5
MenorOnas <153731241+MenorOnas@users.noreply.github.com>
2023-12-13 20:27:38 +0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1, 
+        question: "What is the chemical symbol for gold?",
+        answer: "A. Au",
+        options: [
+            "A. Au",
+            "B. Ag",
+            "C. Fe",
+            "D. Cu"
+        ]
+    },
+    {
+        numb: 2, 
+        question: "Which planet is known as the Red Planet?",
+        answer: "B. Mars",
+        options: [
+            "A. Venus",
+            "B. Mars",
+            "C. Jupiter",
+            "D. Saturn"
+        ]
+    },
+    {
+        numb: 3, 
+        question: "What is the powerhouse of the cell?",
+        answer: "B. Mitochondria",
+        options: [
+            "A. Nucleus",
+            "B. Mitochondria",
+            "C. Ribosome",
+            "D. Endoplasmic reticulum"
+        ]
+    },
+    {
+        numb: 4, 
+        question: "Which of the following is not a primary color of light?",
+        answer: "D. Yellow",
+        options: [
+            "A. Red",
+            "B. Blue",
+            "C. Green",
+            "D. Yellow"
+        ]
+    },
+    {
+        numb: 5, 
+        question: "What is the largest mammal on Earth?",
+        answer: "B. Blue whale",
+        options: [
+            "A. Elephant",
+            "B. Blue whale",
+            "C. Giraffe",
+            "D. Rhinoceros"
+        ]
+    },
+];
